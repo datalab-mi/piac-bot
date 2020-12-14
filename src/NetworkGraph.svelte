28f7cc99f184dfb1d52e342ac9ffc7ffba031b1b
cristianpb <felipebrokate@gmail.com>
2020-12-14 11:47:52 +0100

    💄 Change network graph title

--- src/NetworkGraph.svelte
+++ src/NetworkGraph.svelte
@@ -27,7 +27,7 @@
       simulation = d3.forceSimulation(nodes)
         .force("link", d3.forceLink(links).id(d => d.id))
       // .force("charge", d3.forceManyBody())
-        .force('charge', d3.forceManyBody().strength(-100))
+        .force('charge', d3.forceManyBody())
         .force("center", d3.forceCenter(width / 2, height / 2))
 
       svg = d3.select(".chart")
@@ -69,7 +69,7 @@
         .attr('y', 3);
 
       node.append("title")
-        .text(function(d) { return d.id; });
+        .text(function(d) { return `${d.text} - ${d.question}`; });
 
       simulation
         .nodes(nodes)
